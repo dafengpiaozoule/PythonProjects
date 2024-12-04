7ceaef20f283059e467299d354f30dca00efef21
dafengpiaozoule <3212066485@qq.com>
2024-12-04 12:21:39 +0800

    软工测试数据作业第二个

---
+++ tesr.py
@@ -0,0 +1,25 @@
+x = None
+y = None
+z = None
+
+
+def yuju1(x, z):
+    z /= x
+
+def yuju2(z):
+    z += 1
+
+
+def yuju3(z, y):
+    z += y
+
+
+if x > 0 & y > 0:
+    yuju1(x,z)
+    if x > 1 | z >1:
+        yuju2(z)
+    yuju3(z, y)
+else:
+    if x > 1 | z >1:
+        yuju2(z)
+    yuju3(z, y)
