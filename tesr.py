726ecef12deac6b97da3fbfab25cf40a7c61f68a
dafengpiaozoule <3212066485@qq.com>
2024-12-04 15:41:03 +0800

    list列表的常用功能练习

--- tesr.py
+++ tesr.py
@@ -1,25 +1,12 @@
-x = None
-y = None
-z = None
+#常用功能
 
-
-def yuju1(x, z):
-    z /= y
-
-def yuju2(z):
-    z += 1
-
-
-def yuju3(z, y):
-    z += y
-
-
-if x > 0 & y > 0:
-    yuju1(x,z)
-    if x > 1 | z >1:
-        yuju2(z)
-    yuju3(z, y)
-else:
-    if x > 1 | z >1:
-        yuju2(z)
-    yuju3(z, y)
+list_bank = [21, 25, 21, 23, 22, 20]
+list_bank.append(31)
+print(list_bank)
+list_bank.extend([29, 33, 30])
+print(list_bank)
+print(list_bank.pop(0))
+print(list_bank)
+print(list_bank.pop(-1))
+print(list_bank)
+print(list_bank.index(31))
